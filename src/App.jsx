9a76157056c1bd7c533de09b80f3319a06ad5a74
whdagency <138483447+whdagency@users.noreply.github.com>
2024-03-27 11:26:41 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 import { RouterProvider, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { router } from "./router/index";
 import Login from "./authentification/page";
-
+import Dashboard from "./pages/dashboard/page";
 function App() {
   return (
     <Router>
       <Routes>
-       <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
